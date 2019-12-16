b91fce4b5216c315335ecf2507c5e4b6cc065ced
avyukd <avyukd@gmail.com>
2019-12-15 19:40:12 -0500

    Adding 1] pose estimation model 2] pre-processing code 3] all external files necessary for openpose to run. Basically everything up to running and testing the model.

---
+++ generate_poses.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Pose_Process import process
+
+test_frames = np.load("frames_1-10.npy")
+raw_data = [(x['name'],x['image'],x['isFight']) for x in test_frames]
+pose_to_fight = []
+counter = 0
+for n in raw_data[0::10]:
+    canvas = process(n[1])
+    pose_to_fight.append({'name':n[0],'image':canvas,'isFight':n[2]})
+    print(counter)
+    counter+=1
+    if counter % 100 == 0:
+        np.save('frames_1-10_input_data', pose_to_fight)
--- test_openpose.py
+++ test_openpose.py
@@ -12,12 +12,26 @@ import time
 import msgpack
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import Pose_Process
+from Pose_Process import process
+#import sys
 
+#args = sys.argv
 
+#test_frames = args[1]
+#print("hello")
+from config_reader import config_reader
+
+#params, model_params = config_reader()
 test_frames = np.load("frames_1-10.npy")
 #print(test_frames.shape)
 #print(test_frames[0]['image'].shape)
 #print(test_frames[0]['image'])
-
-plt.imshow(test_frames[0]['image'])
+#plt.imshow(test_frames[0]['image'])
+#plt.show()
+raw_data = [(x['image'],x['isFight']) for x in test_frames]
+plt.imshow(process(raw_data[0][0]))
 plt.show()
+#plt.imshow(test_frames[0]['image'])
+#plt.show()
+print(np.array(raw_data).shape)
